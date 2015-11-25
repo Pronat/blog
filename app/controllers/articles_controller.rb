daa5c187b45b21dc86c2660f645f3cf56dba69e0
Alexandr Suvorov <Fotolinza@gmail.com>
2015-11-25 20:20:11 +0300

    implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,16 @@ class ArticlesController < ApplicationController
 	def new
 	end
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		if@article.valid?
+			@article.save  #если валидация true, то возвращаем app/views/article/create.html.erb
+		else
+			render action: 'new'	#запускаем метод def new (action)
+		end
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
 	end
 end
