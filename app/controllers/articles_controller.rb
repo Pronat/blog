50bb775ead94c4db24f566ad2a59e009e9892796
Alexandr Suvorov <Fotolinza@gmail.com>
2015-11-25 22:02:23 +0300

    Add index method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,21 @@
 class ArticlesController < ApplicationController
+
+	def index
+		@articles = Article.all	
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
+
 	def create
 		@article = Article.new(article_params)
 		if@article.valid?
 			@article.save  #если валидация true, то возвращаем app/views/article/create.html.erb
-			redirect_to @articles
+			redirect_to @article
 		else
 			render action: 'new'	#запускаем метод def new (action)
 		end
